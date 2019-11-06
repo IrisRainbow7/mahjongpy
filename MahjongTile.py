85fbf0781c683b213ba19108cd850cb0953677d0
Irisrainbow7 <a17.bnxk@g.chuo-u.ac.jp>
2019-11-06 23:23:31 +0900

    麻雀牌138枚セット生成関数を追加

--- MahjongTile.py
+++ MahjongTile.py
@@ -5,12 +5,39 @@ class MahjongTile:
     NUMBER_READING_JP = {1:'イー', 2:'リャン', 3:'サン', 4:'スー', 5:'ウー', 6:'ロー', 7:'チー', 8:'パー', 9:'キュー'}
     TILE_READING_JP = {'manzu':'ワン', 'pinzu':'ピン', 'souzu':'ソー', 'ton':'トン', 'nan':'ナン', 'sha':'シャー', 'pei':'ペイ', 'haku':'ハク', 'hatu':'ハツ', 'tyun':'チュン'}
     TILE_DISPLAY = {'manzu':'萬', 'pinzu':'●', 'souzu':'Ⅰ', 'ton':'東', 'nan':'南', 'sha':'西', 'pei':'北', 'haku':'白', 'hatu':'發', 'tyun':'中'}
-    def __init__(self, tile_type, number, aka_dora=False):
+
+    def __init__(self, tile_type, number=1, akadora=False):
         if not tile_type in self.TILE_TYPES: raise ValueError('unknown tile types')
         if not number in [i for i in range(1,10)]: raise ValueError('unknown tile types')
         self.tile_type = tile_type
         self.number = number
         if tile_type in self.TILE_TYPES_ZIHAI: self.number = None
-        self.aka_dora = aka_dora
+        self.akadora = akadora
         self.name_jp = self.NUMBER_READING_JP[number] + self.TILE_READING_JP[tile_type]
-        self.display = str(number) + self.TILE_DISPLAY[tile_type]
+        self.display = str(self.number) + self.TILE_DISPLAY[tile_type]
+        if self.number is None: self.display = self.TILE_DISPLAY[tile_type]
+
+    @classmethod
+    def make_tiles_set(use_akadora=True):
+        tiles = []
+        for _ in range(4):
+            for i in range(1,10):
+                tiles.append(MahjongTile('manzu',i))
+                tiles.append(MahjongTile('souzu',i))
+                tiles.append(MahjongTile('pinzu',i))
+            tiles.append(MahjongTile('ton'))
+            tiles.append(MahjongTile('nan'))
+            tiles.append(MahjongTile('sha'))
+            tiles.append(MahjongTile('pei'))
+            tiles.append(MahjongTile('haku'))
+            tiles.append(MahjongTile('hatu'))
+            tiles.append(MahjongTile('tyun'))
+        if use_akadora:
+            tiles.pop(12)
+            tiles.pop(13)
+            tiles.pop(14)
+            tiles.append(MahjongTile('manzu',5,akadora=True))
+            tiles.append(MahjongTile('souzu',5,akadora=True))
+            tiles.append(MahjongTile('pinzu',5,akadora=True))
+        return(tiles)
+
