c88b846ea286e41e47935c65f80dda9d37e08f55
Irisrainbow7 <a17.bnxk@g.chuo-u.ac.jp>
2019-11-18 17:08:42 +0900

    PyPI登録用のsetupを追加

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'mahjongpy',
+    packages = ['mahjongpy'],
+    version = '0.1.0',
+    license = 'MIT',
+    install_requires = [],
+    author = 'Irisrainbow7',
+    author_email = '',
+    url = 'https://github.com/Irisrainbow7/mahjongpy',
+    description = 'Japanese-style mahjong library for python',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    keywords = 'mahjongpy mahjong',
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: Japanese',
+        ],
+    )
